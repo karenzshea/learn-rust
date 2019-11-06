d0b498544936a7cad997522e041a1fa9685a0874
karen shea <karenzshea@gmail.com>
2019-11-06 10:44:15 +0100

    fix stuff

--- pqueue/src/main.rs
+++ pqueue/src/main.rs
@@ -9,32 +9,32 @@ struct PriorityQueue {
 }
 
 impl PriorityQueue {
-    fn insert(mut &self, &item: Item) -> bool {
+    fn insert(&mut self, item: &Item) -> bool {
         return false;
     }
 
     fn pop(&self) -> &Item {
-        return Item{};
+        return &Item{priority: 0, item: 0};
     }
 
-    fn deleteHighestPriority(mut &self) -> &Item {
-        return Item{};
-    }
+    fn deleteHighestPriority(&mut self) -> Item {
+        return Item{priority: 0, item: 0};
     }
 
-    fn decreaseKey(mut &self, &id: u32, &new_priority: u32) -> bool {
+    fn decreaseKey(&mut self, id: u32, new_priority: u32) -> bool {
         return false;
     }
 
-    fn increaseKey(mut &self, &id: u32, &new_priority: u32) -> bool {
+    fn increaseKey(&mut self, id: u32, new_priority: u32) -> bool {
         return false;
     }
 
     fn empty(&self) -> bool {
-        self.heap.size() == 0
+        self.heap.len() == 0
     }
 }
 
 fn main() {
+    let mut values : Vec<Item>;
     println!("Hello, world!");
 }
